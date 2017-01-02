ca803bd3a10961b9de7049440091a8821fa6b4ad
user2 <user2@user2-PC>
2017-01-02 21:14:43 +0900

    dsa

--- Spring/src/spring/di1/UserBean.java
+++ Spring/src/spring/di1/UserBean.java
@@ -1,14 +1,14 @@
 package spring.di1;
 
-import org.springframework.beans.factory.annotation.Required;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class UserBean {
+	/*의존관계를 저절로 설정*/
+	@Autowired
+	@Qualifier("userService1")
 	private UserService userService;
 
-	@Required
-	public void setUserService(UserService userService) {
-		this.userService = userService;
-	}
 	
 	public void execute() {
 		System.out.println(userService.msg());
--- Spring/src/spring/di1/UserServiceImpl2.java
+++ Spring/src/spring/di1/UserServiceImpl2.java
@@ -2,7 +2,7 @@ package spring.di1;
 
 import org.springframework.beans.factory.annotation.Required;
 
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl2 implements UserService{
 	private String name;
 	private String tel;
 	private int age;
@@ -26,7 +26,7 @@ public class UserServiceImpl implements UserService{
 	@Override
 	public String msg() {
 		// TODO Auto-generated method stub
-		return name+":"+tel+":"+age;
+		return "이름:"+name+":"+",전화:"+tel+":"+",나이:"+age;
 	}
 
 }
